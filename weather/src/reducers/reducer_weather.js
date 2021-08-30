11875f3a6b68fe47337e0052933d685eb2424532
Naba <naba.1923ec1005@kiet.edu>
2021-08-30 23:24:06 +0530

    initial commit

---
+++ weather/src/reducers/reducer_weather.js
@@ -0,0 +1,17 @@
+import {FETCH_WEATHER} from '../actions/index'
+
+
+
+export default function (state=[], action) {
+    console.log(state)
+    switch (action.type) {
+        case FETCH_WEATHER:        
+            return [ ...state, action.payload.data ]
+            
+
+            default:
+            return state; 
+    }
+
+    
+}
