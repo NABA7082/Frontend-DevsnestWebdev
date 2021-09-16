baf644c397bc0530b9e622f841297d627236fab3
NABA7082 <69723668+NABA7082@users.noreply.github.com>
2021-09-16 22:58:04 +0530

    added

--- reducers/index.ts
+++ reducers/index.ts
@@ -1,7 +1,12 @@
+//reducers
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
     user: userReducer,
+
+
 });
+
+
 export default rootReducer;
